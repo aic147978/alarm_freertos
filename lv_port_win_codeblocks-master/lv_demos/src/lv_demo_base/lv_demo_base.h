73a04eccefa9ef97e0e0390bba435b9c2f9a8bad
aic147978 <aic306619@gmail.com>
2025-09-17 09:59:18 +0800

    6

--- lv_port_win_codeblocks-master/lv_demos/src/lv_demo_base/lv_demo_base.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LV_DEMO_BASE_H
-
-#define _LV_DEMO_BASE_H
-
-#include "lvgl/lvgl.h"
-
-void lv_demo_base(void);
-
-#endif
--- lv_port_win_codeblocks-master/myGUI.h
+++
@@ -1,7 +0,0 @@
-#ifndef _MYGUI_H
-#define _MYGUI_H
-#include "lvgl/lvgl.h"
-
-void my_GUI(void);
-#endif // _MYGUI_H
-
